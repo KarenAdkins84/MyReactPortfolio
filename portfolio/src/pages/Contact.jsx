4109d1e53873ed9573621ff7744ec5bf0d6274aa
Nerakma84 <fnk8476@gmail.com>
2022-12-30 03:22:56 -0600

    added 2 more projects to portfolio

--- portfolio/src/pages/Contact.jsx
+++ portfolio/src/pages/Contact.jsx
@@ -6,15 +6,15 @@ const Contact = () => {
         <form>
         <div className="form-group">
             <label htmlFor="nameInput">Name: </label>
-            <input type="text" id="nameInput" placeholder="First and Last Name"/>
+            <input type="text" id="nameInput" className='form-control' placeholder="First and Last Name" required />
             </div>
             <div className="form-group">
             <label htmlFor="emailAddressInput">Email: </label>
-            <input type="email" id="emailAddressInput" placeholder="name@example.com"/>
+            <input pattern='/^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/' type="email" id="emailAddressInput" className='form-control' placeholder="name@example.com" required/>
             </div>
             <div className="form-group">
             <label htmlFor="textInput">Message: </label>
-            <textarea id="textInput"></textarea>
+            <textarea id="textInput" required ></textarea>
             </div>
             <div className='button'>
             <button>Submit</button>
